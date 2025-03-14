70b05f0e740aac2b724686daea009ecc046ae57e
Manisha Rana <manisharana02005@gmail.com>
2025-03-14 11:15:06 +0530

    Created all the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 
+import React from "react"
+import Navbar from "./components/Navbar"
+import Filter from "./components/Filter"
+import Cards from "./components/Cards"
+import {apiUrl, filterData} from "./data"
 
 function App() {
 
   return (
-    <>
-     <h1>Top Courses</h1>
-    </>
+    <div>
+      <Navbar/>
+
+      <Filter
+        filterData={filterData}
+      />
+
+      <Cards/>
+    </div>
   )
 }
 
---
+++ src/components/Filter.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+function Filter({filterData}) {
+  return (
+    <div>
+        {filterData.map((data)=>{
+            return( <button>
+                {data.title}
+            </button>)
+        })}
+    </div>
+  )
+}
+
+export default Filter
